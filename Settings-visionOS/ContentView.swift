87e41bb870ab2894e4962e9035b60c9a7387aefd
Chris <zhris@zhr.is>
2024-01-02 16:52:42 -0600

    Initial Commit

---
+++ Settings-visionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Settings-visionOS
+//
+//  Created by Chris on 1/2/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Settings-visionOS/Settings_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Settings_visionOSApp.swift
+//  Settings-visionOS
+//
+//  Created by Chris on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Settings_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
