d08ca57a813e12f09dd6aec968f4823b777fc3b7
Chris <zhris@zhr.is>
2024-01-02 19:39:21 -0600

    Modified ContentView to begin adding functionality.

--- Settings-visionOS/ContentView.swift
+++ Settings-visionOS/ContentView.swift
@@ -10,14 +10,67 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+    enum Setting: String, CaseIterable, Identifiable {
+        case general = "General"
+        case apps = "Apps"
+        
+        var id: String { rawValue }
+        
+        var icon: String {
+            switch self {
+            case .general:
+                return "gear.circle.fill"
+            case .apps:
+                return "questionmark.circle.fill"
+            }
         }
-        .padding()
+        
+        var color: Color {
+            switch self {
+            case .general:
+                return .gray
+            case .apps:
+                return .blue
+            }
+        }
+        
+        var destination: some View {
+            switch self {
+            case .general:
+                return AnyView(EmptyView())
+            case .apps:
+                return AnyView(EmptyView())
+            }
+        }
+    }
+    
+    @State private var searchText = ""
+    @State private var selection: Setting? = .general
+
+    var body: some View {
+        NavigationSplitView {
+            List(selection: $selection) {
+                ForEach(Setting.allCases) { setting in
+                    NavigationLink(value: setting, label: {
+                        Image(systemName: setting.icon)
+                            .font(.largeTitle)
+                            .fontWeight(.light)
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(.white.gradient, setting.color.gradient)
+                        Text(setting.id)
+                    })
+                }
+                
+                NavigationLink("Label", destination: EmptyView())
+            }
+            // Find a way to hide navigation bar title but keep the search bar
+            .navigationBarTitle("Settings")
+            .navigationBarHidden(false)
+            // Change search bar to rounded
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        } detail: {
+            selection?.destination
+        }
     }
 }
 
