a928fb7eed085b332fd755a8ad42ffc9851fd02b
Chris Pineda <zhris@zhr.is>
2024-01-10 20:13:54 -0600

    Added NotificationsView.

---
+++ Settings-visionOS/Views/NotificationsView.swift
@@ -0,0 +1,50 @@
+//
+//  NotificationsView.swift
+//  Settings-visionOS
+//
+//  Created by Chris on 1/10/24.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                ListRowNavigationLabel(title: "Screen Sharing", subtitle: "Notifications Off", content: AnyView(EmptyView()))
+                
+                Section(content: {
+                    NavigationLink(destination: {}, label: {
+                        HStack(alignment: .center) {
+                            Image(systemName: "questionmark.circle.fill")
+                                .font(.largeTitle)
+                            VStack(alignment: .leading) {
+                                Text("FaceTime")
+                                Text("Banners, Sounds, Badges")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    })
+                }, header: {
+                    Text("Notification Style")
+                })
+            }
+            .padding([.leading, .trailing], 25)
+            .navigationTitle("Notifications")
+            .toolbar {
+                ToolbarItem(placement: .principal, content: {
+                    HStack {
+                        Text("Notifications")
+                            .font(.title)
+                    }
+                    .frame(maxWidth: .infinity)
+                })
+            }
+        }
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
