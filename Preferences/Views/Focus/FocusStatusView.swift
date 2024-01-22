e84b2b3dc3987d327ccf9a052f961291a4a24108
Chris Pineda <zhris@zhr.is>
2024-01-22 15:18:14 -0600

    Added FocusStateView with its destination in FocusView.

---
+++ Preferences/Views/Focus/FocusStatusView.swift
@@ -0,0 +1,47 @@
+//
+//  FocusStatusView.swift
+//  Preferences
+//
+//  Settings > Focus > Focus Status
+//
+
+import SwiftUI
+
+struct FocusStatusView: View {
+    var body: some View {
+        List {
+            Section(content: {}, footer: {
+                Text("\u{201C}Share Across Devices\u{201D} must be turned on to share your Focus state to this device.")
+            })
+            
+            Section(content: {
+                Toggle("Share Focus Status", isOn: .constant(true))
+            }, footer: {
+                Text("When a Focus is on, apps that you allow can show that you have notifications silenced.")
+            })
+            .disabled(true)
+            
+            Section(content: {
+                Toggle("Do Not Disturb", isOn: .constant(true))
+                Toggle("Mindfulness", isOn: .constant(true))
+                Toggle("Personal", isOn: .constant(true))
+                Toggle("Sleep", isOn: .constant(true))
+                Toggle("Work", isOn: .constant(true))
+            }, header: {
+                Text("Share From")
+            }, footer: {
+                Text("Customize which Focus profiles can share that you have notifications silenced.")
+            })
+            .disabled(true)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading, content: {
+                Text("Focus Status").font(.title2)
+            })
+        }
+    }
+}
+
+#Preview {
+    FocusStatusView()
+}
