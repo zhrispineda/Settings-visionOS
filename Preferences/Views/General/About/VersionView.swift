036b1c49ef8ffc7938074919b65ca4d09e783ae9
Chris <zhris@zhr.is>
2025-04-01 16:50:30 -0400

    Update to 2.4

--- Preferences/Views/General/About/VersionView.swift
+++ Preferences/Views/General/About/VersionView.swift
@@ -12,7 +12,7 @@ struct VersionView: View {
         CustomList(title: "\(UIDevice().systemName) Version") {
             Section {
                 VStack(alignment: .leading, spacing: 10) {
-                    Text("\(UIDevice().systemName) \(UIDevice().systemVersion) (22N906)")
+                    Text("\(UIDevice().systemName) \(UIDevice().systemVersion) (22O238)")
                         .fontWeight(.heavy)
                     Text("This update includes improvements and bug fixes for your \(UIDevice().name).")
                         .foregroundStyle(.secondary)
