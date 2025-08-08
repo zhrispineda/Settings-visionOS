ae49d06baad9630977d0b3809811ec8eb5cdd029
Chris <zhris@zhr.is>
2025-08-08 11:22:39 -0400

    Update AccountsUI asset usage

--- Preferences/Views/Shared/AddAccountView.swift
+++ Preferences/Views/Shared/AddAccountView.swift
@@ -9,30 +9,30 @@
 import SwiftUI
 
 struct AddAccountView: View {
-    let services = ["icloud", "exchange", "gmail", "yahoo-white", "aol-white", "Outlook_Logo"]
+    let accountsPath = "/System/Library/PrivateFrameworks/AccountsUI.framework"
+    let services = ["iCloud", "Exchange", "Gmail", "Yahoo", "AOL", "Outlook"]
     
     var body: some View {
         CustomList(title: "Add Account") {
-            ForEach(services, id: \.self) { service in
-                HStack {
-                    Image(service)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 50)
-                        .frame(maxWidth: .infinity)
+            Section("Choose your provider") {
+                ForEach(services, id: \.self) { service in
+                    HStack {
+                        if let image = UIImage.asset(path: accountsPath, name: service) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
                 }
-            }
-            // Other: NavigationLink
-            ZStack(alignment: .leading) {
-                NavigationLink("", destination: AddOtherAccountView()).opacity(0)
-                HStack {
-                    Spacer()
-                    Text("Other")
-                        .font(.system(size: 24))
+                // Other: NavigationLink
+                Button {} label: {
+                    Text("Add Other Account…")
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         .foregroundStyle(.blue)
-                    Spacer()
                 }
-                .padding(.vertical, 10)
             }
         }
     }
